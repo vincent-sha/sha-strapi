6658d3bd667052f920494cac54fafcd4ade30e7a
vincent-sha <vincentshajing@gmail.com>
2025-12-13 11:58:46 +0800

    plugins.ts里设置相应的字段，开启了导入导出插件

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,23 @@
-export default () => ({});
+export default {
+  'strapi-csv-import-export': {
+    enabled: true,
+    config: {
+      authorizedExports: [
+        'api::blog.blog',
+        'api::author.author',
+        'api::category.category',
+        'api::tag.tag',
+        'api::about.about',
+        'api::global.global'
+      ],
+      authorizedImports: [
+        'api::blog.blog',
+        'api::author.author',
+        'api::category.category',
+        'api::tag.tag',
+        'api::about.about',
+        'api::global.global'
+      ],
+    },
+  },
+};
